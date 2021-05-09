ea9ffa8de366be3d22d2ab0fe22ba8fb4767f0ca
Chris Pauley <cmpauley@gmail.com>
2021-05-09 13:07:23 -0700

    restructure cli

---
+++ wordsiv/cli/package/__init__.py
@@ -0,0 +1,9 @@
+import typer
+
+from . import build
+from . import list as list_packages
+
+app = typer.Typer()
+
+app.command('list')(list_packages.list_packages)
+app.command('build')(build.build_package)
