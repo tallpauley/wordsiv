d82ab61244191be6e74b02a44c3e2c36eb4bd685
Chris Pauley <cmpauley@gmail.com>
2021-05-13 13:01:54 -0700

    changing default seed of WordSiv to generate a fun example sentence

--- tests/run_byexample.sh
+++ tests/run_byexample.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash -e
 
 # temporarily set this to false so we can make a new virtualenv
-poetry config virtualenvs.in-project false
-poetry env remove python3 || true
-poetry env use python3
-poetry run pip install byexample
-poetry run byexample -l shell,python README.md -o "+term=ansi"
+python -m venv ${PWD}/.venv_byexample
+. .venv_byexample/bin/activate
+pip install byexample
+byexample -l shell,python README.md -o "+term=ansi"
+rm -r .venv_byexample
