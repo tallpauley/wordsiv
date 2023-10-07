f9fa4045edfca92f2ad15fbd1ff96f5d3d48ba9f
José Solé <type@jmsole.cl>
2023-10-07 12:38:19 +0100

    Use io library to open model source file

--- wordsiv/sentence_models_sources/wordcount.py
+++ wordsiv/sentence_models_sources/wordcount.py
@@ -5,6 +5,7 @@ import random
 import reprlib
 from pathlib import Path
 import os
+import io
 import re
 import json
 
@@ -51,7 +52,7 @@ def zip_tuple(a_tuple):
 def stream_file_tuples(file, lines=None):
     """Yield a tuple ('myword', 123) for each line of a file like 'myword 123'"""
 
-    with open(file, "r", encoding="utf-8") as f:
+    with io.open(file, "r", encoding="utf-8") as f:
         lc = 0
         for line in f:
             line_items = re.split("\s+", line.strip())
